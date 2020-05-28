46dc149f617bc762aeaa49b76d37a8ae034e3c32
Daniel dos Santos Vaini <vainidaniel@gmail.com>
2020-05-28 14:28:30 -0300

    Atualização modelo e Estilo

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,15 +1,30 @@
 function contar(){
-    var res = document.getElementById('res');
     var inicio = document.querySelector('input#n1');
-    var ini = Number(inicio.value);
     var final = document.querySelector('input#n2')
-    var fim = Number(final.value);
     var passo = document.querySelector('input#n3');
-    var pass = Number(passo.value);
+    let res = document.getElementById('res');
+    if(inicio.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERROR] Faltam dados')
+    }else{
+        res.innerHTML = 'Contando:  <br>'
+        let i = Number(inicio.value);
+        let f = Number(final.value);
+        let p = Number(passo.value);
+        if(p <= 0){
+            window.alert('Passo invalido! Considerando passo igual a 1');
+            p = 1;
+        }
+        if(i < f){
+            for (let c = i; c <= f; c += p){
+                res.innerHTML +=`${c} \u{1F449}	`;
+            }
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1f449}`;
+            }
 
-    res.innerHTML = 'Contando: '
+        }
 
-    for(ini; ini <= fim; ini+= pass){
-        res
+        res.innerHTML += `\u{1f3c1}`
     }
 }
