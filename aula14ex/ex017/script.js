fce24789b4f1c6de31ab689769adb6a5b61c8dd2
Daniel dos Santos Vaini <vainidaniel@gmail.com>
2020-05-28 16:02:52 -0300

    Atualização Estilo, Script e modelo
    Exercicio completo e funcionando

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function calcular(){
+    
+    var n = document.querySelector('input#n1');
+    var res = document.querySelector('div#res')
+    var select = document.querySelector('select#select')
+    if(n.value.length == 0){
+        window.alert('Digite um numero')
+    }else{
+        select.innerHTML = '';
+        var num = Number(n.value);
+        for(var c = 0; c <= 10; c++){
+            var op = document.createElement('option')
+            var mul = c * num;
+            op.text += `${num} x ${c} = ${mul}`
+            select.add(op);
+        }
+    }
+}
