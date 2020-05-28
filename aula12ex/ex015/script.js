3646deb70befc3904555be2a26abf9c1dd950bd1
Daniel dos Santos Vaini <vainidaniel@gmail.com>
2020-05-28 00:15:19 -0300

    Aula 12 modulo D terminada

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -16,26 +16,29 @@ function verificar(){
         if(fsex[0].checked){
             genero = 'Homem';
             if (idade >= 0 && idade < 10){
-
+                img.setAttribute('src', 'foto-bebe-m.png');
             }else if(idade < 21){
-
+                img.setAttribute('src', 'foto-jovem-m.png');
             }else if(idade < 50){
-                
+                img.setAttribute('src', 'foto-adulto-m.png');
             }else{
-
+                img.setAttribute('src','foto-idoso-m.png');
             }
         }else if(fsex[1].checked){
             genero = 'Mulher';
             if(idade >= 0 && idade < 10){
-
+                img.setAttribute('src', 'foto-bebe-f.png');
             }else if(idade < 21){
-
+                img.setAttribute('src', 'foto-jovem-f.png');
             }else if(idade < 50){
-
+                img.setAttribute('src','foto-adulto-f.png');
             }else{
-                
+                img.setAttribute('src', 'foto-idoso-f.png');
             }
         }
+        res.style.textAlign = 'center';
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        res.appendChild(img);
         
     }
 }
