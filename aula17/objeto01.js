036e08bf3b70f3bb84707734651e42a7958a27e5
Daniel dos Santos Vaini <vainidaniel@gmail.com>
2020-06-03 09:37:32 -0300

    Aula 17
    create Objeto.js

---
+++ aula17/objeto01.js
@@ -0,0 +1,13 @@
+let amigo = {nome: 'Daniel',
+sexo: 'M',
+peso: 85.5,
+engordar(p){
+
+    console.log('Engordou')
+    this.peso += p
+
+    }
+}
+
+amigo.engordar(3)
+console.log( `${amigo.nome} pesa ${amigo.peso}`)
